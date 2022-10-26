8b2d0c3b6f47e6eaae7b668f6dcd4659f962c81d
StefanPacola <stevo.pacola@gmail.com>
2022-10-26 15:08:20 +0200

    added text ater match is finished

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const scoreSelector = document.querySelector("#score");
 const playerOne = document.querySelector(".playerOne");
 const playerTwo = document.querySelector(".playerTwo");
 
+const results = document.querySelector(".results");
+const h2 = document.querySelector("h2").textContent.trim();
+
 let playerOneScore = 0;
 let playerTwoScore = 0;
 let isGameOver = false;
@@ -20,9 +23,14 @@ playerOneButton.addEventListener("click", () => {
             isGameOver = true;
             playerOne.classList.add("winner");
             playerTwo.classList.add("loser");
+
+            results.innerHTML = "";
+            let p = document.createElement("p");
+            p.innerHTML = `Zápas skončil výsledkom <br> ${h2}.`
+            results.appendChild(p);
         }
         playerOne.textContent = playerOneScore;
-    }
+    } 
 });
 
 // PLAYER TWO SETTINGS
@@ -54,4 +62,9 @@ function reset (){
     playerTwoScore = 0;
     playerOne.textContent = 0;
     playerTwo.textContent = 0;
+    playerOne.classList.remove("winner");
+    playerOne.classList.remove("loser");
+    playerTwo.classList.remove("winner");
+    playerTwo.classList.remove("loser");
+    results.innerHTML = "";
 }
